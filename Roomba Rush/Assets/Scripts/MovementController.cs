25d8a0eeb5656dafe5e527b30eef203d514b9a97
RobbieBoudreaux <60223949+RobbieBoudreaux@users.noreply.github.com>
2020-02-29 14:47:17 -0500

    Made Roomba

--- Roomba Rush/Assets/Scripts/MovementController.cs
+++ Roomba Rush/Assets/Scripts/MovementController.cs
@@ -4,50 +4,42 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    public Vector3 direction = new Vector3(1, 0, 0);
+
+
+    public enum roombaState
+    {
+        Spinning,
+        Moving,
+        Hit_Ragdoll
+    }
+    roombaState currentState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = roombaState.Spinning;
     }
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
     {
-        gameObject.transform.position += direction * Time.deltaTime;
+        if (currentState == roombaState.Spinning)
+        {
+            gameObject.transform.Rotate(0, 0, .25f, Space.Self);
+        }
+        else if (currentState == roombaState.Moving)
+        {
+            gameObject.transform.position += transform.up * Time.deltaTime;
+        }
+
         if (Input.GetKeyDown(KeyCode.A))
         {
-            if (direction == Vector3.right)
+            if (currentState == roombaState.Moving)
             {
-                direction = Vector3.up;
-            } else if(direction == Vector3.up)
-            {
-                direction = Vector3.left;
-            } else if (direction == Vector3.left)
-            {
-                direction = Vector3.down;
-            } else if (direction == Vector3.down)
-            {
-                direction = Vector3.right;
+                currentState = roombaState.Spinning;
             }
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            if (direction == Vector3.right)
+            else if (currentState == roombaState.Spinning)
             {
-                direction = Vector3.down;
-            }
-            else if (direction == Vector3.up)
-            {
-                direction = Vector3.right;
-            }
-            else if (direction == Vector3.left)
-            {
-                direction = Vector3.up;
-            }
-            else if (direction == Vector3.down)
-            {
-                direction = Vector3.left;
+                currentState = roombaState.Moving;
             }
         }
     }
