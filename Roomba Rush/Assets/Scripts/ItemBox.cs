b1d9a196bce173b0d3fa901c2290b3197792c6e1
JMigneault <jacrovm@gmail.com>
2020-03-07 17:30:23 -0500

    Add first version of item boxes.

---
+++ Roomba Rush/Assets/Scripts/ItemBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// todo: make randomized, implement wave itembox spawning.
+public class ItemBox : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        switch (collider.gameObject.tag)
+        {
+            case "Roomba":
+                if (collider.GetComponentInChildren<RoombaItem>() == null)
+                {
+                    Transform item = GetComponentInChildren<RoombaItem>().transform;
+                    item.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+                    item.parent = collider.transform;
+                    item.localPosition = Vector2.zero;
+                    item.rotation = item.parent.rotation;
+                }
+                GameObject.Destroy(this.gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
